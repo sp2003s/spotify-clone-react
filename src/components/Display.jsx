56f04ca0cc6bee7263959f8dd6f26f80060e43be
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-08-12 00:11:12 +0530

    Display Home route

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Display = () => {
   return (
-    <div>
-      
+    <div className='w-[100] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
+
+        <Routes>
+            <Route path='/' element = {<DisplayHome/>} />
+        </Routes>
+
     </div>
   )
 }
