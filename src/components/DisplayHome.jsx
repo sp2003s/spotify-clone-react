77c18c346b7ecc68f84289d6a2566791375b6d4f
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-08-12 18:12:25 +0530

    display home featured charts and remove scroll bar for overflow

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from "react";
+import Navbar from "./Navbar";
+import { albumsData } from "../assets/frontend-assets/assets";
+import AlbumItem from "./AlbumItem";
 
 const DisplayHome = () => {
   return (
     <>
       <Navbar />
 
-      <div className='my-5 font-bold text-2xl'>
-        
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default DisplayHome
+export default DisplayHome;
