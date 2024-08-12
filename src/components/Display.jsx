aaa35ad81bce5e1c92880b222ac8c3d737fb893d
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-08-13 02:20:30 +0530

    display album data

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 import DisplayHome from './DisplayHome';
+import DisplayAlbum from './DisplayAlbum';
 
 const Display = () => {
   return (
@@ -8,6 +9,7 @@ const Display = () => {
 
         <Routes>
             <Route path='/' element = {<DisplayHome/>} />
+            <Route path='/album/:id' element = {<DisplayAlbum/>} />
         </Routes>
 
     </div>
