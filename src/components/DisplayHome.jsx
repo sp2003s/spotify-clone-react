c192d3730ffdadce9b69a537d4a29e89adbbba35
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-08-13 01:52:58 +0530

    display home - song item

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Navbar from "./Navbar";
-import { albumsData } from "../assets/frontend-assets/assets";
+import { albumsData, songsData } from "../assets/frontend-assets/assets";
 import AlbumItem from "./AlbumItem";
+import SongItem from "./SongItem";
 
 const DisplayHome = () => {
   return (
@@ -13,6 +14,21 @@ const DisplayHome = () => {
         <div className="flex overflow-auto">
           {albumsData.map((item, index) => (
             <AlbumItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Today's biggest hits</h1>
+        <div className="flex overflow-auto">
+          {songsData.map((item, index) => (
+            <SongItem
               key={index}
               name={item.name}
               desc={item.desc}
