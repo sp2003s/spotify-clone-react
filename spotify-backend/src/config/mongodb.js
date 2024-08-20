fbcb80f489973fb95570f632272f4d02c592f8dd
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-08-20 16:30:01 +0530

    typo correction in mongodb.js env portion and api logic for addSong in song Controller

--- spotify-backend/src/config/mongodb.js
+++ spotify-backend/src/config/mongodb.js
@@ -5,8 +5,7 @@ const connectDB = async () => {
     mongoose.connection.on('Connected', () => {
         console.log('Connection Established')
     })
-    
-    await mongoose.connect(`${procces.env.MONDODB_URI}/spotify-clone`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/spotify-clone`)
 }
 
 export default connectDB;
