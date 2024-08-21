a4b871e7a3957e11cbb99f85e98d64f898d9776b
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-08-21 22:21:20 +0530

    album controller function declaration and album router

---
+++ spotify-backend/src/controllers/albumController.js
@@ -0,0 +1,16 @@
+import {vs as cloudinary} from 'cloudinary';
+import albumModel from '../models/albumModel.js';
+
+const addAlbum = async (req, res) => {
+
+}
+
+const listAlbum = async (req, res) => {
+
+}
+
+const removeAlbum = async (req, res) => {
+
+}
+
+export {addAlbum, listAlbum, removeAlbum} 
