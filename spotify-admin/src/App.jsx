de57aad91de2d5d770c3271dd671c7b1dc0be419
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-09-03 00:36:41 +0530

    creating routes for app.jsx to all 4 pages

--- spotify-admin/src/App.jsx
+++ spotify-admin/src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {Routes, Route} from 'react-router-dom'
+import AddSong from './pages/AddSong'
+import AddAlbum from './pages/AddAlbum'
+import ListSong from './pages/ListSong'
+import ListAlbum from './pages/ListAlbum'
 
 const App = () => {
   return (
-    <div>
+    <div className='flex item-start min-h-screen'>
+
+      <ToastContainer />
+
+      <div className='flex-1 h-screen overflow-y-scroll bg-[#F3FFF7]'>
+
+        <div className='pt-8 pl-5 sm:pt-12 sm:pl-12'>
+          <Routes>
+            <Route path = '/add-song' element={<AddSong/>} />
+            <Route path = '/add-album' element={<AddAlbum/>} />
+            <Route path = '/list-song' element={<ListSong/>} />
+            <Route path = '/list-album' element={<ListAlbum/>} />
+          </Routes>
+        </div>
+      </div>
       
     </div>
   )
