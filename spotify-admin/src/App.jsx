426c3ed314495ecbe7d748b6f7032dfc17e2281d
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-09-04 22:51:18 +0530

    Adding navbar to app.jsx

--- spotify-admin/src/App.jsx
+++ spotify-admin/src/App.jsx
@@ -7,6 +7,7 @@ import AddAlbum from './pages/AddAlbum'
 import ListSong from './pages/ListSong'
 import ListAlbum from './pages/ListAlbum'
 import Sidebar from './components/Sidebar';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
@@ -17,7 +18,7 @@ const App = () => {
       <Sidebar />
 
       <div className='flex-1 h-screen overflow-y-scroll bg-[#F3FFF7]'>
-
+        <Navbar />
         <div className='pt-8 pl-5 sm:pt-12 sm:pl-12'>
           <Routes>
             <Route path = '/add-song' element={<AddSong/>} />
