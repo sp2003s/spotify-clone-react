41a3d8b44bb5525cccfed8c939da88870c0ffb10
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-09-07 23:03:13 +0530

    admin - pages - AddAlbum.jsx - Page layout

--- spotify-admin/src/pages/AddAlbum.jsx
+++ spotify-admin/src/pages/AddAlbum.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const AddAlbum = () => {
   return (
-    <div>
+    <form className='flex flex-col items-start gap-8 text-gray-600'>
+
+      <div className='flex flex-col gap-4'>
+        <p>Upload Image</p>
+        <input type="file" id='image' accept='image/*' hidden/>
+        <label htmlFor="image">
+          <img className='w-24 cursor-pointer' src={assets.upload_area} alt="" />
+        </label>
+      </div>
+
+      <div className='flex flex-col gap-2.5'>
+        <p>Album Name</p>
+        <input className='bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw,250px)]' type="text" placeholder='Type Here'/>
+      </div>
+
+      <div className='flex flex-col gap-2.5'>
+        <p>Album Description</p>
+        <input className='bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw,250px)]' type="text" placeholder='Type Here'/>
+      </div>
+
+      <div className='flex flex-col gap-3'>
+        <p>Background Color</p>
+        <input type="color" />
+      </div>
+
+      <button className='text-base bg-black text-white py-2.5 px-14 cursor-pointer' type='submit'>ADD</button>
       
-    </div>
+    </form>
   )
 }
 
