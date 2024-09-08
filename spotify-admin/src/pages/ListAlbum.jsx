0605e37a2935d362a3f1e7649396289dcaec931d
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-09-09 00:18:55 +0530

    admin, ListAlbums.jsx, fetchAlbum function

--- spotify-admin/src/pages/ListAlbum.jsx
+++ spotify-admin/src/pages/ListAlbum.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useActionData } from 'react-router-dom';
+import { url } from '../App';
+import { toast } from 'react-toastify';
 
 const ListAlbum = () => {
+
+  const [data, setData] = useState([]);
+
+  const fetchAlbums = async () => {
+    try {
+
+      const response = await axios.get(`${url}/api/album/list`)
+
+      if(response.data.success){
+        setData(response.data.albums)
+      }
+      
+    } catch (error) {
+      toast.error('Error Occoured')
+    }
+  }
+
   return (
     <div>
       
