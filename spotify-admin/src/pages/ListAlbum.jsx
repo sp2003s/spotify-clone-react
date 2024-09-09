f198445039721e9080dbc0f1c48062a6d73b5131
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-09-09 16:03:47 +0530

    admin, listAlbum, div and content shown for all albums

--- spotify-admin/src/pages/ListAlbum.jsx
+++ spotify-admin/src/pages/ListAlbum.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useActionData } from 'react-router-dom';
 import { url } from '../App';
 import { toast } from 'react-toastify';
@@ -28,7 +28,33 @@ const ListAlbum = () => {
 
   return (
     <div>
-      
+      <p>All Albums List</p>
+
+      <br />
+
+      <div>
+
+        <div className='sm:grid hidden grid-cols-[0.5fr_1fr_2fr_1fr_0.5fr] items-center gap-2.5 p-3 border border-gray-300 text-sm mr-5 bg-gray-100'>
+          <b>Image</b>
+          <b>Name</b>
+          <b>Description</b>
+          <b>Album Colour</b>
+          <b>Action</b>
+        </div>
+
+        {data.map((item, index) => {
+          return (
+            <div key = {index} className='grid grid-cols-[1fr_1fr_1fr] sm:grid-cols-[0.5fr_1fr_2fr_1fr_0.5fr] items-center gap-2.5 p-3 border border-gray-300 text-sm mr-5'>
+              <img className='w-12' src={item.image} alt="" />
+              <p>{item.name}</p>
+              <p>{item.desc}</p>
+              <input type="color" value={item.bgColor}/>
+              <p className='cursor-pointer'>X</p>
+            </div>
+          )
+        })}
+
+      </div>
     </div>
   )
 }
